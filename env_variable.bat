e37f64bbdc3b5906d1c58a65c50a0c0f1b59872a
Luqman Hakim <luqman.hakim.pens@gmail.com>
2020-05-14 13:32:24 +0700

    add .gitignore; modify env_variable comment;

--- env_variable.bat
+++ env_variable.bat
@@ -1,10 +1,10 @@
 :: set installed NRF5 SDK path
 SET SDK_ROOT=D:\Nordic\nRF5SDK16-0098a08e2
 
-:: set compiled application hex path
+:: set your compiled application hex path
 SET APP_HEX=D:\Nordic\nRF5SDK16-0098a08e2\examples\ble_peripheral\ble_app_beacon\pca10040\s132\armgcc\_build\nrf52832_xxaa.hex
 
-:: set precompiled Nordic's SDK path
+:: set where precompiled Nordic's sofdevice path
 SET SOFTDEVICE_HEX=%SDK_ROOT%\components\softdevice\s132\hex\s132_nrf52_7.0.1_softdevice.hex
 
 :: set softdevice_code, refer to "nrfutil pkg generate --help" command
